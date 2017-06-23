d58d17b41588b89a741d58b8ea0148f54706fad7
srics96 <sricharan14312@cse.ssn.edu.in>
2017-06-23 22:46:32 +0530

    Added models and push tasks to db

--- PGE/models.py
+++ PGE/models.py
@@ -1,5 +1,74 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from datetime import datetime, timedelta
+from django.utils import timezone
 
-# Create your models here.
+
+class Role(models.Model):
+    
+    WEB_FRONT_DEVELOPER = "WFD"
+    BACKEND_DEVELOPER = "BD"
+    ANDROID_DEVELOPER = "AD"
+    iOS_DEVELOPER = "ID"
+    DESIGNER = "DS"
+
+    ROLE_CHOICES = (
+        (WEB_FRONT_DEVELOPER, "WebFrontendDeveloper"),
+        (BACKEND_DEVELOPER, "BackendDeveloper"),
+        (ANDROID_DEVELOPER, "AndroidDeveloper"),
+        (iOS_DEVELOPER, "iOSDeveloper"),
+        (DESIGNER, "Designer"),
+    )
+
+    role_name = models.CharField(choices=ROLE_CHOICES, max_length=3, default=ANDROID_DEVELOPER  , unique=True)
+
+    def __str__(self):
+        return self.role_name
+
+
+class Employee(models.Model):
+    name = models.CharField(max_length=20, default=None)
+    email = models.EmailField(max_length=30, unique=True)
+    roles = models.ManyToManyField(Role)
+
+    
+    def __str__(self):
+        return self.name
+
+
+class Manager(models.Model):
+    employee_instance = models.OneToOneField(Employee)
+
+
+class Project(models.Model):
+    project_name = models.CharField(max_length=100, default=None)
+    start_date = models.DateField(default=datetime.now().date())
+    end_date = models.DateField(default=None, null=True)
+    manager = models.OneToOneField(Manager)
+    employees = models.ManyToManyField(Employee)
+    
+    def __str__(self):
+        return self.project_name
+
+
+class Task(models.Model):
+    task_name = models.CharField(max_length=100, default=None)
+    start_date = models.DateField(default=datetime.now().date())
+    deadline = models.DateField(default=datetime.now().date() + timedelta(days=30))
+    employees = models.ManyToManyField(Employee)
+    dimensions = models.ManyToManyField(Role)
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.task_name
+
+
+
+
+    
+    
+    
+
+
+
--- PGE/views.py
+++ PGE/views.py
@@ -2,12 +2,14 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
 
+from PGE.models import Employee, Manager, Project, Role, Task
 import json
 
 # Utility method to delete unicodes
 
 TASK_ADDITION_KEY_PROJECT_NAME = "project_name"
 TASK_ADDITION_KEY_TASKS = "tasks"
+TASK_ADDITION_MANAGER_EMAIL = "manager_email"
 
 def byteify(input):
     if isinstance(input, dict):
@@ -19,13 +21,25 @@ def byteify(input):
         return input.encode('utf-8')
     else:
         return input
+
+
 @csrf_exempt
 def add_tasks(request):
     if request.method == 'POST':
+        
         recieved_json = json.loads(request.body)
-        task_dict = byteify(recieved_json)
-        for key, value in task_dict.items():
-            print (key, value)
+        input_dict = byteify(recieved_json)
+        manager_email = input_dict[TASK_ADDITION_MANAGER_EMAIL]
+        employee_obj = Employee.objects.get(email=manager_email)
+        manager_obj, created = Manager.objects.get_or_create(employee_instance=employee_obj)
+        project_name = input_dict[TASK_ADDITION_KEY_PROJECT_NAME]
+        project_obj = Project(project_name=project_name, manager=manager_obj)
+        project_obj.save()
+        task_names = input_dict[TASK_ADDITION_KEY_TASKS]
+        for task in task_names:
+            task_obj = Task(task_name=task, project=project_obj)
+            task_obj.save()
+
         response = {
             "message" : "successfull"
         }
