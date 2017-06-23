db7cb73fd4e953c70cf8a999f8383e206c7ab1e0
srics96 <sricharan14312@cse.ssn.edu.in>
2017-06-23 13:03:18 +0530

    urls error fix

--- PGE/urls.py
+++ PGE/urls.py
@@ -4,6 +4,6 @@ from django.conf.urls import url
 from PGE.views import add_tasks, handle_message 
 
 urlpatterns = [
-	url(r'^submitTasks', add_tasks, name = "add_tasks")
+	url(r'^submitTasks', add_tasks, name = "add_tasks"),
 	url(r'^submitMessage', handle_message, name="handle_message"),
 ]
