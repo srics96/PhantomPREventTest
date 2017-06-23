a2f36b464142a5973506ffdf0708df0bddac6de0
srics96 <sricharan14312@cse.ssn.edu.in>
2017-06-23 15:29:04 +0530

    unicode fix

--- PGE/views.py
+++ PGE/views.py
@@ -15,7 +15,7 @@ def byteify(input):
                 for key, value in input.items()}
     elif isinstance(input, list):
         return [byteify(element) for element in input]
-    elif isinstance(input, unicode):
+    elif isinstance(input, bytes):
         return input.encode('utf-8')
     else:
         return input
